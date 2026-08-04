f2d44ad6bf455c6b1bb5df920895660541debf88
prayagDev <88964135+prayagDev@users.noreply.github.com>
2026-08-04 06:53:33 +0530

    Add files via upload

---
+++ sql/2/main.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT
+    emp.name AS Employee
+FROM Employee emp
+JOIN Employee manager
+    ON emp.managerId = manager.id
+WHERE emp.salary > manager.salary;
