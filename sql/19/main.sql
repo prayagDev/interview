2687799c05faf56139d8dc3faff51c2486e5c5bd
prayagDev <88964135+prayagDev@users.noreply.github.com>
2026-08-26 08:05:29 +0530

    Update main.sql

--- sql/19/main.sql
+++ sql/19/main.sql
@@ -1,12 +1,13 @@
 SELECT
     person_name
 FROM
-(
-    SELECT
-        person_name,
-        SUM(weight) OVER (ORDER BY turn) AS total_weight
-    FROM Queue
-) t
-WHERE total_weight <= 1000
-ORDER BY total_weight DESC
-LIMIT 1;
+    (
+        SELECT
+            person_name,
+            SUM(weight) OVER (ORDER BY turn) AS running_total
+        FROM Queue
+    ) q
+WHERE running_total <= 1000
+ORDER BY running_total DESC
+LIMIT 1
+;
